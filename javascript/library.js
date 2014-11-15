af6bc6f737e58a92bddfbd3c13a327e2ad336aed
Susanne Dao <susanne.dao@gmail.com>
2014-11-15 16:14:39 -0500

    quotation script fix

--- javascript/library.js
+++ javascript/library.js
@@ -4,23 +4,23 @@ function ChangeQuote() {
   
   var quotes = [
   	{
-  		quotation: "A common mistake that people make when trying to design something completely foolproof is to underestimate the ingenuity of complete fools.<br />",
+  		quotation: "A common mistake that people make when trying to design something completely foolproof is to underestimate the ingenuity of complete fools.",
   		author: "Douglas Adams"
   	},
   	{
-  		quotation: "Don't look at your feet to see if you are doing it right. Just dance.<br />",
+  		quotation: "Don't look at your feet to see if you are doing it right. Just dance.",
   		author: "Anne Lamott"
   	},
   	{
-  		quotation: "We can judge our progress by the courage of our questions and the depth of our answers, our willingness to embrace what is true rather than what feels good.<br />",
+  		quotation: "We can judge our progress by the courage of our questions and the depth of our answers, our willingness to embrace what is true rather than what feels good.",
   		author: "Carl Sagan"
   	},
   	{
-  		quotation: "Knowing how to be solitary is central to the art of loving. When we can be alone, we can be with others without using them as a means of escape.<br />",
+  		quotation: "Knowing how to be solitary is central to the art of loving. When we can be alone, we can be with others without using them as a means of escape.",
   		author: "bell hooks"
   	},
     {
-      quotation: "It is good to love many things, for therein lies the true strength, and whosoever loves much performs much, and can accomplish much, and what is done in love is well done.<br />",
+      quotation: "It is good to love many things, for therein lies the true strength, and whosoever loves much performs much, and can accomplish much, and what is done in love is well done.",
       author: "Vincent van Gogh"
     }
   ];
@@ -32,7 +32,7 @@ function ChangeQuote() {
   }
   
   function quote(q) {
-  	return q.quotation + '<span class="name">' + q.author + '</span>';
+  	return q.quotation + '<br /><span class="name">' + q.author + '</span>';
   }
 
   new_quote.innerHTML = rand(quotes).map(quote)[0];
